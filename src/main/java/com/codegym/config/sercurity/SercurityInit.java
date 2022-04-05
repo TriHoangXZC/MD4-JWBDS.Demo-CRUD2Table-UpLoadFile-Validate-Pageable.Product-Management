69208c510b933b880eddd8fc39ada1fd7a278c33
TriHoangZXC <hoanghuutri97@gmail.com>
2022-04-05 14:47:29 +0700

    add sercurity

---
+++ src/main/java/com/codegym/config/sercurity/SercurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.config.sercurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SercurityInit extends AbstractSecurityWebApplicationInitializer {
+}
